f5c65ae85362c9e902c4bdbd7d5a9ef00c5f7014
Brandon Reynolds <brandonreynolds1227@gmail.com>
2025-09-22 18:20:39 -0700

    Libary to Library, added .gitignore

--- adaptive-libary-system/src/alms/interfaces.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Searchable(ABC):
-    @abstractmethod
-    def matches(self, text):
-        """Return True if this object matches the search text."""
-        pass
